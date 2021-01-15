1c7ef24821a489cb12f97e5c731189db5e1a38fe
rnick1 <rnick0260@gmail.com>
2021-01-15 14:36:23 -0800

    Set up the welcome/instruction page and start button.

---
+++ script.js
@@ -0,0 +1,42 @@
+// Welcome/instructions card.
+var greeting = "Welcome to the Week 4 Coding Quiz!";
+var 
+
+// These are the question variables.
+var question1 = "What is HTML?";
+var question2 = "What is CSS?";
+var question3 = "What is JavaScript?";
+var question4 = "What are Web API's?";
+var question5 = "What are Third-Party API's?";
+
+// These are the answer variables...solution a for question 1, solution b for question 1, etc.
+var answer1A = "";
+var answer1B = "";
+var answer1C = "";
+var answer1D = "";
+
+var answer2A = "";
+var answer2B = "";
+var answer2C = "";
+var answer2D = "";
+
+var answer3A = "";
+var answer3B = "";
+var answer3C = "";
+var answer3D = "";
+
+var answer4A = "";
+var answer4B = "";
+var answer4C = "";
+var answer4D = "";
+
+var answer5A = "";
+var answer5B = "";
+var answer5C = "";
+var answer5D = "";
+
+
+window.onload.function myFunction() {
+  alert("Page is loaded");
+}
+myFunction();
