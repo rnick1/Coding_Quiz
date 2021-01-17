323e347359c91dfcb41cb36f788a6458d5b61868
rnick1 <rnick0260@gmail.com>
2021-01-16 17:34:14 -0800

    Simplified code...eliminated variables that weren't needed and code that wasn't working.

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// Welcome/instructions card.
-var greeting = "Welcome to the Week 4 Coding Quiz!";
-
-// These are the question variables.
-var question1 = "What is HTML?";
-var question2 = "What is CSS?";
-var question3 = "What is JavaScript?";
-var question4 = "What are Web API's?";
-var question5 = "What are Third-Party API's?";
-
-// These are the answer variables...solution a for question 1, solution b for question 1, etc.
-var answerA = "";
-var answerB = "";
-var answerC = "";
-var answerD = "";
-var answerE = "";
-
-
-var mainField = document.querySelector("h3");
-var contentField = document.querySelector("p");
-var buttonField = document.querySelector("button");
 
 
 // Timer:
@@ -39,16 +18,6 @@ function startTimer() {
     }, 1000);
 
     document.getElementById("quiz-question").innerHTML = "What is HTML?";
-    document.getElementById("quiz-options").innerHTML = "What is HTML?";
-    document.getElementById("start").innerHTML = "Submit Answer";
+
 
 };
-
-function changeText() {
-}
-
-
-
-// mainField.setAttribute("style", "display: none");
-// contentField.setAttribute("style", "display: none");
-// buttonField.setAttribute("style", "display: none");
