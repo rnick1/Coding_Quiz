782e06c3c356061d5ce8d5c9688def200a81a047
rnick1 <rnick0260@gmail.com>
2021-01-17 19:06:57 -0800

    Set up js so that user can move on from the intro to q1 to q2.

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 var introCard = document.querySelector(".intro-card");
-var questionCard = document.querySelector(".question-card");
+var questionCard1 = document.querySelector(".question-card-1");
+var questionCard2 = document.querySelector(".question-card-2");
+var questionCard3 = document.querySelector(".question-card-3");
+var questionCard4 = document.querySelector(".question-card-4");
+var questionCard5 = document.querySelector(".question-card-5");
+
 var conclusionCard = document.querySelector(".conclusion-card");
 
 var quizQuestion = document.querySelector("#quiz-question");
-
-var question1 = "What do web developers use to structure a web page?";
-var question2 = "What do web developers use to set the style of a web page?";
-var question3 = "What do web developers use to set the functionality of a web page?";
-var question4 = "What do web developers use to manipulate a web page using the DOM structure?";
-var question5 = "What can web developers use to their use of JavaScript?";
 
 // Timer:
 
@@ -27,13 +26,24 @@ function startTimer() {
         }
     }, 1000);
 
-introCard.setAttribute("style", "display: none");
-questionCard.setAttribute("style", "display: block");
-
+    introCard.setAttribute("style", "display: none");
+    questionCard1.setAttribute("style", "display: block");
 };
 
 function toQuestion2() {
-    quizQuestion = question2;
+    questionCard1.setAttribute("style", "display: none");
+    questionCard2.setAttribute("style", "display: block");
 }
 
-toQuestion2();
+
+
+// var question3 = "What do web developers use to set the functionality of a web page?";
+// var question4 = "What do web developers use to manipulate a web page using the DOM structure?";
+// var question5 = "What can web developers use to their use of JavaScript?";
+
+// var Q2toQ3 = document.querySelector("#to-question-3");
+// var Q3toQ4 = document.querySelector("#to-question-4");
+// var Q4toQ5 = document.querySelector("#to-question-5");
+// var Q5toFinish = document.querySelector("#to-Finish");
+
+// document.getElementById('generate').addEventListener('click', writePassword);
