ebd93197556cc11671c0c61e62729436d67e6986
rnick1 <rnick0260@gmail.com>
2021-01-17 17:54:40 -0800

    Returned to previous version and made big improvements: made three cards, worked on the buttons, and added variables.

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
-var navTimer = querySelector("#timer");
-var introSubmit = querySelector("#introSubmit");
 
-var q1True = document.querySelector("#q1True");
-var q1False1 = document.querySelector("#q1False1");
-var q1False2 = document.querySelector("#q1False2");
-var q1False3 = document.querySelector("#q1False3");
-var q1False4 = document.querySelector("#q1False4");
+var introCard = document.querySelector(".intro-card");
+var questionCard = document.querySelector(".question-card");
+var conclusionCard = document.querySelector(".conclusion-card");
 
-var q2True = document.querySelector("#q2True");
-var q2False1 = document.querySelector("#q2False1");
-var q2False2 = document.querySelector("#q2False2");
-var q2False3 = document.querySelector("#q2False3");
-var q2False4 = document.querySelector("#q2False4");
+var quizQuestion = document.querySelector("#quiz-question");
 
-var q3True = document.querySelector("#q3True");
-var q3False1 = document.querySelector("#q3False1");
-var q3False2 = document.querySelector("#q3False2");
-var q3False3 = document.querySelector("#q3False3");
-var q3False4 = document.querySelector("#q3False4");
+var question2 = "What do web developers use to set the style of a web page?";
 
-var q4True = document.querySelector("#q4True");
-var q4False1 = document.querySelector("#q4False1");
-var q4False2 = document.querySelector("#q4False2");
-var q4False3 = document.querySelector("#q4False3");
-var q4False4 = document.querySelector("#q4False4");
+// Timer:
 
-var q5True = document.querySelector("#q5True");
-var q5False1 = document.querySelector("#q5False1");
-var q5False2 = document.querySelector("#q5False2");
-var q5False3 = document.querySelector("#q5False3");
-var q5False4 = document.querySelector("#q5False4");
+var timeLimit = document.getElementById("time");
 
-var q1Result = document.querySelector(".displayResult1");
-var q2Result = document.querySelector(".displayResult2");
-var q3Result = document.querySelector(".displayResult3");
-var q4Result = document.querySelector(".displayResult4");
-var q5Result = document.querySelector(".displayResult5");
+var secondsLeft = 75;
 
+function startTimer() {
+    timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeLimit.textContent = secondsLeft + " seconds remaining.";
 
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            alert("I am sorry, but you have run out of time. Better luck next time!");
+        }
+    }, 1000);
+
+introCard.setAttribute("style", "display: none");
+questionCard.setAttribute("style", "display: block");
+
+};
+
+function toQuestion2() {
+    quizQuestion.textContent = question2;
+}
+
+toQuestion2();
