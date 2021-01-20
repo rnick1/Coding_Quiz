8d71fa938ac07382466dd0f3e5fc315e98bd62ba
rnick1 <rnick0260@gmail.com>
2021-01-20 14:49:33 -0800

    Fixed issues with having #'s and () where they weren't needed.

--- script.js
+++ script.js
@@ -95,37 +95,33 @@ function setupQuestion4() {
 };
 
 // Current work:
-function setupConclusion() {
-    questionCard.setAttribute('style', 'display: none');
-    conclusionCard.setAttribute('style', 'display: block');
-    clearInterval(timerInterval);
-    // Doesn't do anything
-    saveScore.addEventListener('click', keepScore());
-    // displays the next card
-    goToScore.addEventListener('click', setupScores());
-};
-
 var totalScore = document.querySelector('#score');
 var count = 0;
 var conclusionCard = document.querySelector('#conclusion-card');
 var scoresCard = document.querySelector('#scores-card');
-var saveScore = document.getElementById('#save-score'); 
 var userInitials = document.querySelector('#user-initials');
-var goToScore = document.getElementById('#go-to-score');
+var goToScore = document.getElementById('go-to-score');
 var displayedResult = document.querySelector("#displayed-result");
-// When the object is inside the function, userScore is undefined. When it is outside the function, keepScore is null. 
+var saveScore = document.getElementById('save-score'); 
 
+function setupConclusion() {
+    questionCard.setAttribute('style', 'display: none');
+    conclusionCard.setAttribute('style', 'display: block');
+    clearInterval(timerInterval);
+    goToScore.addEventListener('click', setupScores);
+};
 
-function keepScore() {
+saveScore.addEventListener('click', function(event) {
+event.preventDefault();
+
     var userScore = {
         Initials: userInitials.value,
         Score: count.value,
     };
-    localStorage.setItem('userScore', JSON.stringify(userScore));
-};
-    // localStorage.setItem('Score', JSON.stringify(Score));
-    // userScore.Initials.textContent = userInitials;
-    // userScore.Score.textContent = count;
+localStorage.setItem('userScore', JSON.stringify(userScore));
+
+});
+
 
   function setupScores() {
     conclusionCard.setAttribute('style', 'display: none');
